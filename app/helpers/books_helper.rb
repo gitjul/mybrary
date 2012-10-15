e89aa7a34327bb2a1ff66d7df1a67031dfd82e1c
Julia Jacobs <gjulmail@gmail.com>
2012-10-15 19:38:16 +0200

    Dropdown change status

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,13 +2,13 @@ module BooksHelper
   def toggle_status_info(status = nil)
     case status
       when "None"
-        "I don't have this"
+        "I don't have this book"
       when "Not interested"
         "I'm not interested"
       when "Read"
-        "I've read this"
+        "I've read this book"
       when "Wanna read"
-        "I want to read this"
+        "I want to read this book"
       when nil
         "Change book status"
     end
