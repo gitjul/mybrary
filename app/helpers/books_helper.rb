039722e9dda5da1e6085451a16e6d3bf68da43ca
Julia Jacobs <gjulmail@gmail.com>
2012-10-15 17:36:28 +0200

    Display book status

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,16 @@
 module BooksHelper
+  def toggle_status_info(status = nil)
+    case status
+      when "None"
+        "I don't have this"
+      when "Not interested"
+        "I'm not interested"
+      when "Read"
+        "I've read this"
+      when "Wanna read"
+        "I want to read this"
+      when nil
+        "Change book status"
+    end
+  end 
 end
