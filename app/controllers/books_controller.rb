29ce70caa04e0aef29bf53e51a76e89c65078fd6
Julia Jacobs <gjulmail@gmail.com>
2012-10-16 23:16:50 +0200

    Change books per page number

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,16 @@
 class BooksController < ApplicationController
+  BPP = 10
+
   def index
     if params[:search]
       @books = Book.search params[:search],
-                  :page => params[:page], :per => 4,
+                  :page => params[:page], :per => BPP,
                   :star => true,
                   :match_mode => :boolean,
                   :sort_mode => :extended,
                   :order => "@relevance DESC, created_at DESC"
     else 
-      @books = Book.page(params[:page]).per(4)
+      @books = Book.page(params[:page]).per(BPP)
     end
   end
 
@@ -17,19 +19,19 @@ class BooksController < ApplicationController
   end
 
   def to_read
-    @books = Book.to_read.page(params[:page]).per(4)
+    @books = Book.to_read.page(params[:page]).per(BPP)
   end
 
   def read
-    @books = Book.read.page(params[:page]).per(4)
+    @books = Book.read.page(params[:page]).per(BPP)
   end
 
   def not_interested
-    @books = Book.not_interested.page(params[:page]).per(4)
+    @books = Book.not_interested.page(params[:page]).per(BPP)
   end
 
   def dont_have
-    @books = Book.dont_have.page(params[:page]).per(4)
+    @books = Book.dont_have.page(params[:page]).per(BPP)
   end
 
   def change_status
